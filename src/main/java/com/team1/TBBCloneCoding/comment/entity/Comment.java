88302b4dc4ec06799312c4b0c2645611c6f0a7d2
jeongo <jinism2736@gmail.com>
2022-12-25 17:36:40 +0900

    [feat] Comment - Entity 변경
    1. Comment 조회 시, 변경순으로 불러오기 위해 TimeStamp 필드 추가하였습니다
    2. #5 브랜치에서 Member 필드 추가한 내용이 이번 작업에도 반영되었습니다

--- src/main/java/com/team1/TBBCloneCoding/comment/entity/Comment.java
+++ src/main/java/com/team1/TBBCloneCoding/comment/entity/Comment.java
@@ -1,16 +1,19 @@
 package com.team1.TBBCloneCoding.comment.entity;
 
+import com.team1.TBBCloneCoding.common.entity.TimeStamp;
+import com.team1.TBBCloneCoding.member.entity.Member;
 import com.team1.TBBCloneCoding.project.entity.Project;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class Comment {
+public class Comment extends TimeStamp {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,13 +22,25 @@ public class Comment {
     @Column(nullable = false)
     private String contents;
 
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
     @ManyToOne
     @JoinColumn(name = "Project_Id", nullable = false)
     private Project project;
 
+    @ManyToOne
+    @JoinColumn(name = "Member_Id", nullable = false)
+    private Member member;
+
+
     @Builder
-    public Comment(String contents, Project project) {
+    public Comment(String contents, Project project, Member member) {
         this.contents = contents;
         this.project = project;
+        this.member = member;
     }
 }
