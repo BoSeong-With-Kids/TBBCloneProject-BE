671f5ca9edc434b250e82ca276d2824cc002c579
Inventor <developer7098@gmail.com>
2022-12-26 10:57:12 +0900

    [fix] feature-#12 픽스 - 수정 및 주석 삭제

--- src/main/java/com/team1/TBBCloneCoding/project/service/ProjectService.java
+++ src/main/java/com/team1/TBBCloneCoding/project/service/ProjectService.java
@@ -1,11 +1,7 @@
 package com.team1.TBBCloneCoding.project.service;
 
 import com.team1.TBBCloneCoding.common.dto.ResponseDto;
-import com.team1.TBBCloneCoding.member.entity.Member;
-import com.team1.TBBCloneCoding.project.dto.ProjectCreateRequestDto;
-import com.team1.TBBCloneCoding.project.dto.ProjectUpdateRequestDto;
 import com.team1.TBBCloneCoding.project.entity.Project;
-import com.team1.TBBCloneCoding.project.mapper.ProjectMapper;
 import com.team1.TBBCloneCoding.project.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,29 +13,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProjectService {
     private final ProjectRepository projectRepository;
-    private final ProjectMapper projectMapper;
-    @Transactional
-    public ResponseDto createProject(ProjectCreateRequestDto projectCreateRequestDto, Member member) {
-        Project project = projectMapper.toEntity(projectCreateRequestDto, member);
-        projectRepository.save(project);
-        return new ResponseDto("success","프로젝트 등록에 성공했습니다.",null);
-    }
-
-    @Transactional
-    public ResponseDto updateProject(Long projectId, ProjectUpdateRequestDto projectUpdateRequestDto, Member member) {
-        Project project = projectRepository.findById(projectId).orElseThrow(
-                () -> new IllegalArgumentException("projectId로 데이터베이스에서 프로젝트를 찾을 수 없습니다.")
-        );
-
-        if(!member.getLoginId().equals(project.getMember().getLoginId())){
-            throw new IllegalArgumentException("프로젝트를 생성한 사용자가 아닙니다.");
-        }
-
-        project.update(projectUpdateRequestDto);
-        return new ResponseDto("success","프로젝트 수정에 성공했습니다.",null);
-
-    }
-
     @Transactional(readOnly = true)
     public ResponseDto getProjectList(String filter, String category) {
 
