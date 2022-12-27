d9a3bce55ed660126c9740f143b94ff9aa27bb00
Inventor <developer7098@gmail.com>
2022-12-27 11:36:33 +0900

    [feat] 프로젝트 추가기능 (화) - 중간저장

--- src/main/java/com/team1/TBBCloneCoding/project/service/ProjectService.java
+++ src/main/java/com/team1/TBBCloneCoding/project/service/ProjectService.java
@@ -16,8 +16,8 @@ public class ProjectService {
     @Transactional(readOnly = true)
     public ResponseDto getProjectList(String filter, String category) {
 
-        // 기본값 : 최신순 filter(latest)
-        List<Project> projectList = projectRepository.findAllByCategoryOrderByCreatedAtDesc(category);
+
+        List<Project> projectList;
         // filter에 따라서 정렬순서변경
         if(filter.equals("oldest")){
             // 오래된순
@@ -27,6 +27,14 @@ public class ProjectService {
             // 인기순
            projectList = projectRepository.findAllByCategoryOrderByRecommendCountDesc(category);
         }
+        // 기본값 : 최신순 filter(latest)
+        projectList = projectRepository.findAllByCategoryOrderByCreatedAtDesc(category);
+
+        // projectList에서 project를 뽑아서 projectListResponseDto로 변환해서 전달, 매퍼 사용 X ( 머지가 되지 않은 상황에서 변경 최소화하기 위한 이유 )
+        for(Project project : projectList){
+
+        }
+
         return new ResponseDto("success", "프로젝트 리스트 조회에 성공했습니다.", projectList);
     }
 }
