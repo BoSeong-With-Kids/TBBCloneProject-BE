774d0f0f31803cff7ac5c4849d4bf596bbf18d15
jeongo <jinism2736@gmail.com>
2022-12-28 11:10:09 +0900

    MVP완성 전, 오류 코드 수정
    1. CommentController의 유저 정보 매개변수를 MEMber member -> UserDetailsImpl userDetailsImpl 로 변경
    2. Comment 조회에서  조회순서를 ModifiedAtDesc -> CreatedAtDesc 로 변경

--- src/main/java/com/team1/TBBCloneCoding/comment/controller/CommentController.java
+++ src/main/java/com/team1/TBBCloneCoding/comment/controller/CommentController.java
@@ -5,9 +5,11 @@ import com.team1.TBBCloneCoding.comment.dto.CommentCreateRequestDto;
 import com.team1.TBBCloneCoding.comment.service.CommentService;
 import com.team1.TBBCloneCoding.common.dto.ResponseDto;
 import com.team1.TBBCloneCoding.member.entity.Member;
+import com.team1.TBBCloneCoding.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,36 +28,36 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/project/{projectId}/comment")
-    public ResponseEntity<ResponseDto> createComment(Member member, @PathVariable Long projectId, @RequestBody CommentCreateRequestDto commentCreateRequestDto) {
+    public ResponseEntity<ResponseDto> createComment(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, @PathVariable Long projectId, @RequestBody CommentCreateRequestDto commentCreateRequestDto) {
 
-        ResponseDto responseDto = commentService.createComment(member, projectId, commentCreateRequestDto);
+        ResponseDto responseDto = commentService.createComment(userDetailsImpl, projectId, commentCreateRequestDto);
 
-        return new ResponseEntity(responseDto, HttpStatus.OK) ;
+        return new ResponseEntity(responseDto, HttpStatus.OK);
     }
 
 
     @GetMapping("/project/{projectId}/comment")
-    public ResponseEntity<ResponseDto> getAllComment(Member member, @PathVariable Long projectId) {
+    public ResponseEntity<ResponseDto> getAllComment(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, @PathVariable Long projectId) {
 
-        ResponseDto responseDto = commentService.getAllComment(member, projectId);
+        ResponseDto responseDto = commentService.getAllComment(userDetailsImpl, projectId);
 
         return new ResponseEntity(responseDto, HttpStatus.OK);
     }
     
     
     @PutMapping("/project/{projectId}/comment/{commentId}")
-    public ResponseEntity<ResponseDto> updateComment(Member member, @PathVariable Long commentId, @RequestBody CommentCreateRequestDto commentCreateRequestDto) {
+    public ResponseEntity<ResponseDto> updateComment(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, @PathVariable Long commentId, @RequestBody CommentCreateRequestDto commentCreateRequestDto) {
 
-        ResponseDto responseDto = commentService.updateComment(member, commentId, commentCreateRequestDto);
+        ResponseDto responseDto = commentService.updateComment(userDetailsImpl, commentId, commentCreateRequestDto);
         
         return new ResponseEntity(responseDto, HttpStatus.OK);
      }  
      
 
     @DeleteMapping("/project/{projectId}/comment/{commentId}")
-    public ResponseEntity<ResponseDto> deleteComment(Member member, @PathVariable Long commentId){
+    public ResponseEntity<ResponseDto> deleteComment(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, @PathVariable Long commentId){
 
-        ResponseDto responseDto = commentService.deleteComment(member, commentId);
+        ResponseDto responseDto = commentService.deleteComment(userDetailsImpl, commentId);
 
         return new ResponseEntity(responseDto, HttpStatus.OK);
     }
