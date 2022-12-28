b9ef896798396d70c80345e12f45b32ae2bd81ea
jeongo <jinism2736@gmail.com>
2022-12-28 15:51:11 +0900

    [fix] Comment - 조회 시, UserDetailsImpl 을 매개변수로 받는 코드 수정
    - UserDetailsImpl ->  token만 받는 것으로 변경
    - Service단 로직 수정

--- src/main/java/com/team1/TBBCloneCoding/comment/service/CommentService.java
+++ src/main/java/com/team1/TBBCloneCoding/comment/service/CommentService.java
@@ -11,9 +11,13 @@ import com.team1.TBBCloneCoding.member.repository.MemberRepository;
 import com.team1.TBBCloneCoding.project.entity.Project;
 import com.team1.TBBCloneCoding.project.repository.ProjectRepository;
 import com.team1.TBBCloneCoding.security.UserDetailsImpl;
+import com.team1.TBBCloneCoding.security.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +33,8 @@ public class CommentService {
     private final ProjectRepository projectRepository;
 
     private final CommentMapper commentMapper;
+
+    private final JwtUtil jwtUtil;
 
 
     @Transactional
@@ -49,26 +55,54 @@ public class CommentService {
 
 
     @Transactional(readOnly = true)
-    public ResponseDto getAllComment(UserDetailsImpl userDetailsImpl, Long projectId) {
+    public ResponseDto getAllComment(HttpServletRequest request, Long projectId) {
 
-        Long memberId = userDetailsImpl.getMember().getMemberId();
+        Project project = projectRepository.findById(projectId).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 게시물 입니다.")
+        );
 
-        Boolean commentIsMine = false;
+        Member member = memberRepository.findById(project.getMember().getMemberId()).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 유저입니다.")
+        );
+
+        Boolean commentIsMine;
+
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
 
         List<CommentResponseDto> allCommentResponseDto = new ArrayList<>();
 
         List<Comment> comments = commentRepository.findAllByOrderByCreatedAtDesc();
 
-        for(Comment comment : comments) {
-
-            if(comment.getMember().getMemberId().equals(memberId)){
-                commentIsMine = true;
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                // 토큰에서 사용자 정보 가져오기
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("Token Error");
             }
-            allCommentResponseDto.add(commentMapper.toResponseDto(comment,commentIsMine));
+
+            for (Comment comment : comments) {
+
+                if (comment.getMember().getLoginId().equals(claims.getSubject())) {
+                    commentIsMine = true;
+                } else {
+                    commentIsMine = false;
+                    allCommentResponseDto.add(commentMapper.toResponseDto(comment, commentIsMine));
+                }
+            } return new ResponseDto("success", "댓글 리스트 조회 성공", allCommentResponseDto);
+
+        }  else {
+                for (Comment comment : comments) {
+
+                    commentIsMine = false;
+                    allCommentResponseDto.add(commentMapper.toResponseDto(comment, commentIsMine));
+                }
+            } return new ResponseDto("success", "댓글 리스트 조회 성공", allCommentResponseDto);
         }
 
-        return new ResponseDto("success","댓글을 조회하였습니다", allCommentResponseDto);     
-     }
+
+
      
      
     @Transactional
