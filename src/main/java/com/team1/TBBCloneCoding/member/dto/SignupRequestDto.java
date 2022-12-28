78a0d77dc4faa5478ab81fb27a1f49043bdd4fab
Inventor <developer7098@gmail.com>
2022-12-28 13:51:16 +0900

    [fix] 프로젝트 이미지 업로드 해결 - @RequestMapping("/api/tumblebug") - @JoinColumn(name = "Project_Id", nullable = true) nullable : false -> true

--- src/main/java/com/team1/TBBCloneCoding/member/dto/SignupRequestDto.java
+++ src/main/java/com/team1/TBBCloneCoding/member/dto/SignupRequestDto.java
@@ -11,10 +11,10 @@ import javax.validation.constraints.Pattern;
 @Getter
 public class SignupRequestDto {
     @NotBlank
-    @Pattern(regexp="^[a-zA-Z0-9+-\\_.]$")
+    //@Pattern(regexp="^[a-zA-Z0-9+-\\_.]$")
     private String loginId;
     @NotBlank
-    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,}")
+    //@Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,}")
     private String password;
     @NotBlank
     private String nickname;
