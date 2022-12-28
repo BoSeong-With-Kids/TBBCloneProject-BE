d40249746ae9d3426d65d1fc9d8ce3a8c11ddaa4
jeongo <jinism2736@gmail.com>
2022-12-28 13:27:46 +0900

    [fix] S3ApiController 의 RequestMapping 누락된 부분 코드 수정

--- src/main/java/com/team1/TBBCloneCoding/s3/S3ApiController.java
+++ src/main/java/com/team1/TBBCloneCoding/s3/S3ApiController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
@@ -21,6 +18,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/tumblebug")
 public class S3ApiController {
     private final S3ApiService s3ApiService;
 
