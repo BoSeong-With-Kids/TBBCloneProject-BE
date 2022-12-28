f4c0e0a5013c9e07ec84774524af23c205816179
Inventor <developer7098@gmail.com>
2022-12-28 13:54:01 +0900

    [fix] 백엔드 멤버 정규표현식 에러발생 - 프론트엔드 요청으로 우선 변경하고 dev로 머지요청

--- src/main/java/com/team1/TBBCloneCoding/member/dto/SignupRequestDto.java
+++ src/main/java/com/team1/TBBCloneCoding/member/dto/SignupRequestDto.java
@@ -11,10 +11,8 @@ import javax.validation.constraints.Pattern;
 @Getter
 public class SignupRequestDto {
     @NotBlank
-    @Pattern(regexp="^[a-zA-Z0-9+-\\_.]$")
     private String loginId;
     @NotBlank
-    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,}")
     private String password;
     @NotBlank
     private String nickname;
